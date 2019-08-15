063f276803490eedb1430300457d4a977ccc2750
Daniyil Glushko <danmatrix94@gmail.com>
2019-08-16 02:14:37 +0300

    More operations, and 0xCB instruction table added Timing and instruction size is now being defined in the table as well

---
+++ src/cpu/mod.rs
@@ -0,0 +1,5 @@
+mod operations;
+mod cpu;
+
+pub use cpu::LR35902;
+pub use cpu::INS_TABLE;
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
 use gameboy_emulator::mmu::MMU;
 use gameboy_emulator::cpu::LR35902;
+use std::fs::File;
+use std::io::{Read};
+
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut mmu  = {
+        let mut mmu = MMU::new();
+        let mut file = File::open("D:/hobby/DMG_ROM.bin").expect("Couldn't find the file");
+        let mut buf = Vec::new();
+        file.read_to_end(&mut buf).expect("Failed to read the file");
+        mmu.initialize(buf);
+        mmu
+    };
+
     let mut cpu = LR35902::new();
-    cpu.execute(0x02);
+    for _ in 0..4 {
+        cpu.execute(&mut mmu);
+        println!("CPU state: {:?}", cpu);
+    }
     Ok(())
 }
